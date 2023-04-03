35839187c195d697444c7142691bea1ddd91cee8
ivan.todorovic@octopus.cloud <ivan.todorovic@octopus.cloud>
2023-04-03 11:29:04 +0200

    Allow low letter command

--- GBattleships/Game/FireCommand.cs
+++ GBattleships/Game/FireCommand.cs
@@ -27,6 +27,16 @@ namespace GBattleships.Game
             { 'H', 7 },
             { 'I', 8 },
             { 'J', 9 },
+            { 'a', 0 },
+            { 'b', 1 },
+            { 'c', 2 },
+            { 'd', 3 },
+            { 'e', 4 },
+            { 'f', 5 },
+            { 'g', 6 },
+            { 'h', 7 },
+            { 'i', 8 },
+            { 'j', 9 }
         };
 
         private Dictionary<char, int> _xValues = new Dictionary<char, int>()
